17250041bbc233422964337cf3091befa0f1629f
meltphace7 <brd9326@protonmail.com>
2023-11-20 16:08:43 -0800

    added Tieton construction logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Image from 'next/image';
 import logo from "../../../public/icons/BDLOGO-white.png";
+import tietonLogo from "../../../public/icons/tieton_logo.svg";
 import classes from './Logo.module.css';
 
 const Logo:React.FC = () => {
     return (
         <div className={classes.logo}>
-            <Image src={logo} alt="company logo" className={classes['logo-icon']} />
-            <h1>Test Corp</h1>
+            <Image src={tietonLogo} alt="company logo" className={classes['logo-icon']} />
+            <h1>TIETON</h1>
+            <p>Construction</p>
         </div>
     )
 }
